f7996dbb5bcc7a10f404325f3984641f87898002
Nikhil0108 <124288770+Nikhil0108@users.noreply.github.com>
2025-09-05 13:04:23 +0530

    Update Client.java

--- src/main/java/ChatSockets/Client.java
+++ src/main/java/ChatSockets/Client.java
@@ -6,7 +6,6 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-
   private Socket socket;
   private BufferedReader bufferedReader;
   private BufferedWriter bufferedWriter;
